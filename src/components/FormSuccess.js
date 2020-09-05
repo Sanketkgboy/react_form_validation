f546259ac5fc2c2d9e4cb2f66fb80e1339fbb757
sanketkgboy <sanketkawalesk@gmail.com>
2020-09-05 21:36:44 +0530

    Form validation completed

---
+++ src/components/FormSuccess.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const FormSuccess = () => {
+    return (
+        <div>
+            <h1>Congratulations! You have logged in successfully!</h1>
+        </div>
+    )
+}
